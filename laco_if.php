0577f202c4cd909b71f9ff8b58989581d2e93670
Vinicius <vinicius.s.ribeiro6@aluno.senai.br>
2025-08-14 11:14:01 -0300

    adição do laço e if

---
+++ laco_if.php
@@ -0,0 +1,8 @@
+<?php
+    $resposta = "sim";
+    $pergunta = "Sua mãe deixou vc ir ao MCdonald?";
+
+    if($resposta == "sim"){
+        echo "Vou poder ir ";
+    }else{ echo "hoje não vou poder ir";
+    }
