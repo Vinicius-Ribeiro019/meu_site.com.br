30cdff1127b0214a721f717fa7cb2087715b9cfc
Vinicius <vinicius.s.ribeiro6@aluno.senai.br>
2025-08-14 15:54:43 -0300

    versão dia 14/08/2025

---
+++ arrys.php
@@ -0,0 +1,19 @@
+<?php
+
+    $frutas = [
+        "Maçã",
+        "Banana",
+        "Laranja",
+         "Pera", 
+         "Limão",
+         "Morango"
+        ];
+    
+
+    $i = 0;
+    for ($i = 0; $i <= 5; $i++){
+        echo "frutas : $frutas[$i] <br>";
+    }
+
+
+?>
--- laco_if.php
+++ laco_if.php
@@ -4,5 +4,28 @@
 
     if($resposta == "sim"){
         echo "Vou poder ir ";
-    }else{ echo "hoje não vou poder ir";
+    }else{
+          echo "hoje não vou poder ir";
     }
+    echo "<br>";
+
+    
+    $n1 = 100;
+    $n2 = 200;
+
+    
+
+    if($n1 > $n2)
+        echo "O $n1 é maior que o $n2";
+    if($n2 > $n1)
+        echo "O $n2 é maior que o $n1";
+    if($n1 == $n2)
+        echo "Os dois números são iguais";
+
+    
+
+       
+    
+
+?>
+    
